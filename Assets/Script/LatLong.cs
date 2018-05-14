7b1dd6843af5f8cd027c9a1d51a2fb5cb4b6079f
cp3333 <1772470830@qq.com>
2018-05-14 13:44:41 +0800

    first commit

---
+++ Assets/Script/LatLong.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LatLong : MonoBehaviour {
+
+
+public  float myradius = 5; // globe ball radius
+//经纬度
+public  float mylatitude;
+public float Mylongitude; 
+
+
+
+
+public  void Start()
+{
+  mylatitude = Mathf.PI * mylatitude / 180;
+  Mylongitude = Mathf.PI * Mylongitude / 180;
+
+	// adjust position by radians	
+  mylatitude -= 1.570795765134f; // subtract 90 degrees (in radians)
+
+	// and switch z and y (since z is forward)
+  float xPos = (myradius) * Mathf.Sin(mylatitude) * Mathf.Cos(Mylongitude);
+  float zPos = (myradius) * Mathf.Sin(mylatitude) * Mathf.Sin(Mylongitude);
+  float yPos = (myradius) * Mathf.Cos(mylatitude);
+
+
+	// move marker to position
+  transform.position = new Vector3(xPos, yPos, zPos);
+
+}
+}
